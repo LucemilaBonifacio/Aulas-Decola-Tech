055e19f8309373fcc336046a07c4884bcb5f742b
Lucemila Bonifácio <lucemaylabb@gmail.com>
2025-01-21 10:37:54 -0300

    creat components

---
+++ 07_Angular/app-angular-rh/src/app/components/exemplos/exemplos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exemplos',
+  imports: [],
+  templateUrl: './exemplos.component.html',
+  styleUrl: './exemplos.component.css'
+})
+export class ExemplosComponent {
+
+}
