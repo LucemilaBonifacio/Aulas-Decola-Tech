dedd5edffeedfe94516df6b12bc8dcc45c2b0d78
Lucemila Bonifácio <lucemaylabb@gmail.com>
2025-01-21 11:03:40 -0300

    creat routs

--- 07_Angular/app-angular-rh/src/app/app.routes.ts
+++ 07_Angular/app-angular-rh/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { StartComponent } from './components/start/start.component';
+import { HomeComponent } from './components/home/home.component';
+import { ExemplosComponent } from './components/exemplos/exemplos.component';
+import { CandidatosComponent } from './components/candidatos/candidatos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: StartComponent},
+    {path: 'home', component: HomeComponent},
+    {path: 'exemplos', component: ExemplosComponent},
+    {path: 'candidatos', component: CandidatosComponent}
+];
